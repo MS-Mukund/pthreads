8aeb69c5589d177a439ec0ac41e6ddba010d7d31
MS-Mukund <mukundmeka3456@gmail.com>
2021-11-20 16:29:53 +0530

    coded q1, yet to check for bugs

--- q1.c
+++ q1.c
@@ -10,20 +10,28 @@ int main()
 
     scanf("%d%d%d", &num_students, &num_labs, &num_courses);
 
-    course cs[num_courses];
+    all_courses = (course *)malloc(num_courses * sizeof(course));
+    assert(all_courses != NULL);
 
     for( int i = 0; i < num_courses; i++)
     {
-        scanf("%s%f%d", cs[i].name, &cs[i].interest, &cs[i].num_labs);
+        scanf("%s%f%d", all_courses[i].name, &all_courses[i].interest, &all_courses[i].num_labs);
 
-        cs[i].labs = (int *)malloc(num_labs*sizeof(int));
-        assert(cs[i].labs != NULL );
-        for( int j = 0; j < cs[i].num_labs; j++)
+        all_courses[i].labs = (int *)malloc( num_labs*sizeof(int) );
+        assert(all_courses[i].labs != NULL );
+
+        for( int j = 0; j < all_courses[i].num_labs; j++)
         {
-            scanf("%d", cs[i].labs[j]);
+            scanf("%d", all_courses[i].labs[j]);
         }
 
-        pthread_create(&(cs[i].course_thr_id), NULL, start_c, &(cs[i]) );
+        pthread_cond_init( &(all_courses[i].attend), NULL);
+        pthread_mutex_init( &(all_courses[i].attend_lock), NULL);
+        all_courses[i].num_stud = 0;
+
+        sem_init(&all_courses[i].wait_for_seat, 0, 0);
+        sem_init(&all_courses[i].wait_end_tute, 0, 0);
+        pthread_create(&(all_courses[i].course_thr_id), NULL, start_c, &(all_courses[i]) );
     }
 
     stud st[num_students];
@@ -31,10 +39,35 @@ int main()
     for( int i = 0; i < num_students; i++ )
     {
         st[i].id = st_id++;
-        st[i].state = waiting;
-
+        st[i].state = not_registered;
         scanf("%f%d%d%d%d", &st[i].calibre, &st[i].pref[0], &st[i].pref[1], &st[i].pref[2], &st[i].time);
+
         pthread_create(&(st[i].stud_thread_id), NULL, start_stud, &st[i]);
+    }
+
+    all_labs = (lab *)malloc(num_labs*sizeof(lab));
+    assert(all_labs != NULL);
+
+    lab_locks = (sem_t *)malloc( num_labs*sizeof(sem_t) );
+    assert(lab_locks != NULL);
+
+    for( int i = 0; i < num_labs; i++)
+    {
+        scanf("%s%d%d", all_labs[i].name, &all_labs[i].num_tas, &all_labs[i].max_no_times );
+
+        all_labs[i].num_taed = (int *)malloc( all_labs[i].num_tas*sizeof(int) );
+        assert(all_labs[i].num_taed != NULL );
+
+        all_labs[i].occupied = (bool *)malloc( num_labs*sizeof(bool) );
+        assert(all_labs[i].occupied != NULL );
+
+        for( int j = 0; j < all_labs[i].num_tas; j++)
+        {
+            all_labs[i].num_taed[j] = 0;
+            all_labs[i].occupied[j] = false;
+        }
+
+        sem_init(&(lab_locks[i]), 0, 1);
     }
     
     for( int i = 0; i < num_students; i++ )
@@ -42,46 +75,192 @@ int main()
         pthread_join(st[i].stud_thread_id, NULL);
     }
     
+    for( int i = 0; i < num_courses; i++ )
+    {
+        pthread_cancel(all_courses[i].course_thr_id);
+    }
+
     destroy();
 }
 
 void *start_c(void *args)
 {
     course *c = (course *)args;
-    int cur_ta_id = -1;
-    int cur_ta_lab = -1;
+    
+    while(1)
+    {
+        int status = 0;
+        int cur_ta_id = -1;
+        int cur_lab_id = -1;
+        char *cur_lab_name = NULL;
+        // TA allocation
+        for( int i = 0; i < c->num_labs; i++)
+        {
+            int lab_id = c->labs[i];
+            if( lab_id == -1 )
+            {
+                continue;
+            }
 
+            int lab_stat = 0;       // 1 - lab is free, 0 - lab is full
+            cur_lab_id = lab_id;
 
+            sem_wait( &lab_locks[lab_id] );
+            cur_lab_name = all_labs[lab_id].name;
+
+            for( int j = 0; j < all_labs[lab_id].num_tas; j++)
+            {
+                if( all_labs[lab_id].num_taed[j] < all_labs[lab_id].max_no_times )
+                {
+                    if( all_labs[lab_id].occupied[j] == false )
+                    {
+                        all_labs[lab_id].num_taed[j]++;
+                        cur_ta_id = j;
+                        all_labs[lab_id].occupied[j] = true;
+
+                        status = 1;
+                        break;
+                    }
+
+                    else
+                    {
+                        lab_stat = 1;
+                    }
+                }
+            }
+            sem_post( &lab_locks[lab_id] );
+
+            if( status == 1 )
+            {
+                break;
+            }
+            else if( lab_stat == 0)
+            {
+                print_E12(cur_lab_name);
+                c->labs[i] = -1;
+            }
+        }
+
+        // course withdrawn
+        if( status == 0 )
+        {
+            print_E10(c->name);
+            return NULL;
+        }
+
+        // TA assigned
+        else
+        {
+            print_E11(cur_ta_id, cur_lab_name, c->name);
+
+            int cur_seats = ( rand() % c->max_slots ) + 1;
+            int num_alloc = 0;
+            print_E7(cur_lab_name, cur_seats);
+
+            // allocating seats to students
+            int i;
+            for( i = 0; i < cur_seats; i++ )
+            {
+                // check if any student is waiting for seat
+                int ret = sem_trywait(&c->wait_for_seat);
+                if( ret < 0 && errno == EAGAIN )
+                {   
+                    print_E8(c->name, i, cur_seats);
+                    break;
+                }
+    
+                // wake up student threads waiting for seats
+                sem_post( &c->wait_for_seat );            
+            }
+            num_alloc++;
+
+            // tute
+            sleep(1);
+
+            // end tute
+            acquire(&(c->attend_lock));
+            while( c->num_stud < num_alloc )
+                pthread_cond_wait( &(c->attend), &(c->attend_lock) );
+            release(&(c->attend_lock));
+
+            for( int i = 0; i < num_alloc; i++ )
+            {
+                sem_post( &c->wait_end_tute );
+            }
+
+            // make TA free
+            sem_wait( &lab_locks[cur_lab_id] );
+            all_labs[cur_lab_id].occupied[cur_ta_id] = false;
+            sem_post( &lab_locks[cur_lab_id] );
+            print_E9(cur_ta_id, cur_lab_name, c->name);
+        }
+    } 
 }
 
 void *start_stud(void *args)
 {
     stud *s = (stud *)args;
     int id = s->id;
-    int cur_pref = s->pref[0];
-
-    // if student is not registered yet, keep waiting, give up cpu
-    if( s->time < ticks )
+    
+    // students fills his preferences after 't' seconds
+    if( s->state == not_registered )
     {
-        sched_yield();
+        sem_timedwait(&imaginary_lock, &(s->time));
+        print_E1(id);
     }
+    s->state = waiting;
 
-    if( s->pref[0] == -1)
+    int pref_ind = 0;
+
+    while( pref_ind < 3 )
     {
-        cur_pref = s->pref[1];
-
-        if( s->pref[1] == -1)
+        int ret = sem_wait(&(all_courses[s->pref[pref_ind]].wait_for_seat));
+        if( ret == -1)
         {
-            cur_pref = s->pref[2];
-            if( s->pref[2] == -1)     // student must exit simulation.
+            perror("sem_wait error");
+        }
+        print_E2(id, all_courses[s->pref[pref_ind]].name);
+        s->state = attending;
+
+        acquire( &(all_courses[s->pref[pref_ind]].attend_lock) );
+        all_courses[s->pref[pref_ind]].num_stud++;
+        pthread_cond_signal( &(all_courses[s->pref[pref_ind]].attend) );
+        release( &(all_courses[s->pref[pref_ind]].attend_lock) );
+        ret = sem_wait(&(all_courses[s->pref[pref_ind]].wait_end_tute));
+        if( ret == -1)
+        {
+            perror("sem_wait error");
+        }
+
+        // decide if he wants to continue
+        int rand_num = rand() % 1001;
+        if( rand_num < (all_courses[s->pref[pref_ind]].interest)*(s->calibre)*1000 )
+        {
+            print_E3(id, all_courses[s->pref[pref_ind]].name);
+            pref_ind++;
+            
+            if( pref_ind == 3 )
             {
-                s->state = exited;
-                printf( "Student %d could not get any of his preferred courses\n", id );
+                print_E6(id);
                 return NULL;
             }
+            else
+                print_E4(id, pref_ind, all_courses[ s->pref[pref_ind-1] ].name, all_courses[ s->pref[pref_ind] ].name );
+        }
+        else
+        {
+            print_E5(id, all_courses[ s->pref[pref_ind] ].name);
+            s->state = finished;
+            return NULL;
         }
     }
 
+    // student didn't find any course
+    if( pref_ind >= 3 )
+    {
+        print_E6(id);
+        return NULL;
+    }
 }
 
 void init()
@@ -89,8 +268,8 @@ void init()
     // Initializing semaphores to zero
     for (int i = 0; i < 2; i++)
         sem_init(&(sem[i]), 0, 0); 
-    pthread_mutex_init(&mutex, NULL);
-    pthread_mutex_init(&print_lock, NULL);
+
+    sem_init(&imaginary_lock, 0, 0);
 }
 
 void destroy()
@@ -139,51 +318,51 @@ void print_E5( int id, int course_no )
     pthread_mutex_unlock(&print_lock);
 }
 
-void print_E6( int id, int course_no )
+void print_E6( int id )
 {
     pthread_mutex_lock(&print_lock);
-    printf( "Student %d has been allocated a seat in course %d\n", id, course_no);
+    printf( "Student %d could not get any of his preferred courses\n", id);
     pthread_mutex_unlock(&print_lock);
 }
 
-void print_E7( int id, int course_no )
+void print_E7( char *name, int seats )
 {
     pthread_mutex_lock(&print_lock);
-    printf( "Student %d has been allocated a seat in course %d\n", id, course_no);
+    printf( "Course %s has been allocated %d seats\n", name, seats);
     pthread_mutex_unlock(&print_lock);
 }
 
-void print_E8( int id, int course_no )
+void print_E8( char *name, int filled, int seats)
 {
     pthread_mutex_lock(&print_lock);
-    printf( "Student %d has been allocated a seat in course %d\n", id, course_no);
+    printf( "Tutorial has started for Course %s with %d seats filled out of %d\n", name, filled, seats);
     pthread_mutex_unlock(&print_lock);
 }
 
-void print_E9( int id, int course_no )
+void print_E9( int ta_id, int l_name, char *name )
 {
     pthread_mutex_lock(&print_lock);
-    printf( "Student %d has been allocated a seat in course %d\n", id, course_no);
+    printf( "TA %d from lab %s has completed the tutorial and left the course %s\n", ta_id, l_name, name);
     pthread_mutex_unlock(&print_lock);
 }
 
-void print_E10( int id, int course_no )
+void print_E10( char *name )
 {
     pthread_mutex_lock(&print_lock);
-    printf( "Student %d has been allocated a seat in course %d\n", id, course_no);
+    printf( "Course %s doesn’t have any TA’s eligible and is removed from course offerings\n", name);
     pthread_mutex_unlock(&print_lock);
 }
 
-void print_E11( int id, int course_no )
+void print_E11( int ta_id, char *l_name, char *name )
 {
     pthread_mutex_lock(&print_lock);
-    printf( "Student %d has been allocated a seat in course %d\n", id, course_no);
+    printf( "TA %d from lab %s has been allocated to course %s for nth TA ship\n", ta_id, l_name, name);
     pthread_mutex_unlock(&print_lock);
 }
 
-void print_E12( int id, int course_no )
+void print_E12( int l_name )
 {
     pthread_mutex_lock(&print_lock);
-    printf( "Student %d has been allocated a seat in course %d\n", id, course_no);
+    printf( "Lab %s no longer has students available for TA ship\n", l_name);
     pthread_mutex_unlock(&print_lock);
 }
--- q1.h
+++ q1.h
@@ -6,13 +6,19 @@
 #include <semaphore.h>
 #include <string.h>
 #include <assert.h>
+#include <stdbool.h>
+#include <errno.h>
 
 // Global variables
 // keeps track of number of ticks passed since start of program
 int ticks = 0;
 
 // lock to print to console
-pthread_mutex_t print_lock;
+pthread_mutex_t print_lock = PTHREAD_MUTEX_INITIALIZER;
+
+// semaphores
+sem_t imaginary_lock;
+sem_t *lab_locks;
 
 // Some colors to judge different outputs when running.
 #define NRM "\x1B[1;0m"
@@ -33,23 +39,44 @@ typedef struct course {
 
     int num_labs;   // number of labs from which we can choose;
     int *labs;      // array of lab ids
+
+    sem_t wait_for_seat;
+    sem_t wait_end_tute;
     pthread_t course_thr_id;
+
+    pthread_cond_t attend;
+    pthread_mutex_t attend_lock;
+    int num_stud;
 } course;
 
 enum stud_state { not_registered, waiting, attending, finished, exited };
 typedef struct student {
+
     int id; 
     int time;    // time after which he fills preferences
     int pref[3];  // top 3 preferences
     enum stud_state state;  
     float calibre;      
     pthread_t stud_thread_id;
+
 } stud;
+
+typedef struct lab {
+    char *name;
+    int num_tas;
+    int max_no_times;
+    
+    int *num_taed;      // no of times the TA took tutes
+    bool *occupied;      // is the TA occupied? 
+} lab;
 
 int waiting[2] = {0, 0};        // Number of baboons waiting from each direction
 int travelling[2] = {0, 0};     // Number of baboons crossing the rope (only one of the direction will have positive value at a time)
 sem_t sem[2];
-pthread_mutex_t mutex;          // can also be a binary semaphore. Used to lock critical sections
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;          // can also be a binary semaphore. Used to lock critical sections
+
+lab *all_labs;                 // ptr array of all labs
+course *all_courses;           // ptr array of all courses
 
 void init();
 void destroy();
