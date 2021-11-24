de48513b625d444cea41f7db8e39bd8a69a893eb
MS-Mukund <mukundmeka3456@gmail.com>
2021-11-24 23:23:22 +0530

    added some files for q3

---
+++ q3.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+
+// threads
+#include <pthread.h>
+#include <semaphore.h>
+
+/////////////////////////////
+#include <iostream>
+#include <assert.h>
+#include <tuple>
+using namespace std;
+/////////////////////////////
+
+//Regular bold text
+#define BBLK "\e[1;30m"
+#define BRED "\e[1;31m"
+#define BGRN "\e[1;32m"
+#define BYEL "\e[1;33m"
+#define BBLU "\e[1;34m"
+#define BMAG "\e[1;35m"
+#define BCYN "\e[1;36m"
+#define ANSI_RESET "\x1b[0m"
+
+typedef long long LL;
+
+#define pb push_back
+#define debug(x) cout << #x << " : " << x << endl
+#define part cout << "-----------------------------------" << endl;
+
+///////////////////////////////
+#define MAX_CLIENTS 44
+#define PORT_ARG 8001
+
+const int initial_msg_len = 256;
+const LL buff_sz = 1048576;
+////////////////////////////////////
